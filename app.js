037dbc9a22c8e62b25fcd39b8613033ba547c2c6
Dashamir Hoxha <dashohoxha@gmail.com>
2015-04-14 19:24:48 +0200

    Fixing translation.

--- app.js
+++ app.js
@@ -159,6 +159,9 @@ var _settings = {
         var lng = localStorage.getItem('vocabulary.lng');
         if (lng && lng != 'undefined') {
             $config.lng = lng;
+            if ($config.update_app_language) {
+                _l10n.set_language($config.lng);
+            }
         }
 
         // Load vocabulary.
@@ -176,7 +179,6 @@ var _settings = {
         // Update.
         this.set_title();
         this.update_panel();
-        //$.getScript('l10n/po/sq.js');
     },
 
     set_title: function () {
@@ -210,12 +212,11 @@ var _settings = {
         var tmpl = $('#tmpl-vocabularies').html();
         $('#vocabularies').html(Mustache.render(tmpl, data)).trigger('create');
 
-        // Update layout of the panel.
-        $('#settings').trigger( "updatelayout" );
-
         // Set the value of the keyboard checkbox.
         $('#custom-keyboard').attr('checked', $config.custom_keyboard);
-        $('#custom-keyboard').flipswitch("refresh");
+
+        // Update layout of the panel.
+        $('#settings').trigger('updatelayout');
 
         // Update config and settings when a vocabulary is selected.
         $('.vocabulary').on('click', function () {
@@ -229,6 +230,9 @@ var _settings = {
             for (var v in _options[$config.lng].vocabularies) break;
             $config.vocabulary = v;
             _settings.save();
+            if ($config.update_app_language) {
+                _l10n.set_language($config.lng);
+            }
         });
 
         // Update custom_keyboard when the switch is flipped.
@@ -318,18 +322,26 @@ var _menu = {
 };
 
     
-
 var _l10n = {
-    // Translate some strings.
+    /** Translate the application interface. */
     translate: function () {
-	// Seetings.
+        // Translate seetings.
         $('#lang').html(_('Language'));
         $('#vocab').html(_('Vocabulary'));
         $('label[for="custom-keyboard"').html(_('Use Custom Keyboard:'));
+        $('#done').html(_('Done'));
 
-	// Search.
-	$('#search-term').attr('placeholder', _('Search for a word...'));
-    }
+        // Translate search.
+        $('#search-term').attr('placeholder', _('Search for a word...'));
+
+        // Translate menu.
+        _menu.update();
+    },
+
+    /** Load the translations for the given language. */
+    set_language: function (lng) {
+        $.getScript('l10n/po/' + lng + '.js').done(this.translate);
+    },
 }
 
     
@@ -341,9 +353,9 @@ $(document).on('pagecreate', '#vocabulary', function() {
     // Load language and vocabulary from the local storage.
     _settings.load();
 
-    // Tanslate the interface.
+    // Translate the app interface.
     _l10n.translate();
-    
+
     // Setup menu items.
     _menu.init();
 
@@ -370,8 +382,8 @@ $(document).on('pagecreate', '#vocabulary', function() {
 
     // Initialize Disqus.
     $config.disqus.shortname ?
-	_disqus.init($config.disqus.shortname) :
-	$('#disqus').hide();
+        _disqus.init($config.disqus.shortname) :
+        $('#disqus').hide();
 });
 
     
